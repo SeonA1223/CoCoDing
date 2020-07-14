0c1e2ad2cdb818fc1a0ef80682751203b7aa8ca9
Inyeong Kang <kiyoog02@naver.com>
2020-07-14 15:19:24 +0900

    하단 탭 activity 구현 완료.

---
+++ app/src/main/java/com/example/cocoding/LastActivity.java
@@ -0,0 +1,15 @@
+package com.example.cocoding;
+
+public class LastActivity extends BaseActivity {
+
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_last;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.navigation_last;
+    }
+}
