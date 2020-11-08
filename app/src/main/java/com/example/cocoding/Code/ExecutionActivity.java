4a8564c27087c984d598f0b734f1cbb20fc00628
Seona <bsa14523@gmail.com>
2020-11-08 14:54:25 +0900

    xml 이미지 배치

--- app/src/main/java/com/example/cocoding/Code/ExecutionActivity.java
+++ app/src/main/java/com/example/cocoding/Code/ExecutionActivity.java
@@ -12,19 +12,19 @@ import com.example.cocoding.R;
 
 public class ExecutionActivity extends AppCompatActivity {
 
-    VideoView videoView;
+ //   VideoView videoView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_execution);
 
-        videoView = (VideoView) findViewById(R.id.video_execution);
-        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.execution);
-        //일단 화질은 엄청 깨지지만 영상 대충 찍어서 넣어봤는데 다시 수정해야함
-
-        videoView.setVideoURI(video);
-        videoView.requestFocus();
-        videoView.start();
+//        videoView = (VideoView) findViewById(R.id.video_execution);
+//        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.execution);
+//        //일단 화질은 엄청 깨지지만 영상 대충 찍어서 넣어봤는데 다시 수정해야함
+//
+//        videoView.setVideoURI(video);
+//        videoView.requestFocus();
+//        videoView.start();
     }
 }
--- app/src/main/java/com/example/cocoding/Flow/MakeFlowFragment.java
+++ app/src/main/java/com/example/cocoding/Flow/MakeFlowFragment.java
@@ -53,10 +53,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
+
 
 import org.w3c.dom.Text;
 
